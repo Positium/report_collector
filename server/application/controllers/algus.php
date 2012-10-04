72da6fb738cb4870d6e7716875c16bf821dc3ed4
Mkk <rabakastan@gmail.com>
2012-10-04 10:35:12 +0300

    Server side connection
    Implemented basic data link from mobile ap to database and from database
    to client.

--- server/application/controllers/algus.php
+++ server/application/controllers/algus.php
@@ -5,25 +5,49 @@ class Algus extends CI_Controller {
     function __construct() {
         parent::__construct();
         error_reporting(E_ALL);
-        ini_set('display_errors',1); 
+        ini_set('display_errors',1);
+        $this->load->model('report_post');
     }
     public function index()
     {   
-        if (isset($_POST["nimi"])) {
+        //var_dump($_POST); 
+        if (isset($_POST)) {
+            $json_data = $_POST['photo'];
+            //$jdata = ob_get_clean();
+            //file_put_contents("./uploads/suurus.txt",strlen(http_build_query($_POST)) );
             
-            //No
-            $andmed = "Suurus on: ". $_FILES['userfile']['name'] . "\n";
+            file_put_contents("./uploads/error.txt",$json_data);
+            $ifs=explode(",",$_POST['photo']);
+            
+            $image_string = str_replace(array("\\\\", "''"), array("\\", "'"),pg_escape_bytea(base64_decode($ifs[1])));
+            $report = array(
+                'device_id' => $_POST['uuid'],
+                'timestamp_n' => date("Y-m-d",$_POST['timestamp']),
+                'category' => $_POST['category'],
+                'commentary' => $_POST['comment'],
+                'location_n' => "ST_Transform(ST_GeomFromText('POINT(".$_POST['geolocation']['longitude']." ". $_POST['geolocation']['latitude'].")',4326),900913)",
+                'photo' => $image_string,
+                'gps_accuracy ' => $_POST['geolocation']['accuracy'],
+            );
+            file_put_contents("./uploads/geoinfo.txt",$report['gps_accuracy']);     
+            $this->report_post->insert_report($report);
+         
+            //$data = json_decode($_POST['report']);
+            //$img= imagecreatefromstring($image_string);
+            //imagejpeg($img,'./uploads/suur.jpeg');
+            //$andmed = $data['timestamp'] . "\n";
             //$andmed = $_POST["name"]."\n";
             //$andmed = "proov";
-            $fail = fopen("./uploads/suur",'a');
-            fwrite($fail, $andmed);
-            fclose($fail);
+            //file_put_contents("./uploads/info.txt",$data );
+            //file_put_contents("./uploads/info.jpeg",$image_string);
+            //$fail = fopen("./uploads/suur",'a');
+            //fwrite($fail, $image_string);
+            //fclose($fail);
+
+            //$this->load->view('index.html');
         } else {
-            
-            echo "Ei ole";
+            echo "ei";
         }
-        
-        //$this->load->view('index.html');
     }
 }
 ?>
---
+++ server/application/models/report_post.php
@@ -0,0 +1,55 @@
+<?php
+
+ Class Report_post extends CI_Model
+    {
+     function insert_report($report)
+     {
+        $this -> db ->set('device_id', $report['device_id'], TRUE);
+        $this -> db ->set('timestamp_n', $report['timestamp_n'], TRUE);
+        $this -> db ->set('category', $report['category'], TRUE);
+        $this -> db ->set('commentary', $report['commentary'], TRUE);
+        $this -> db ->set('location_n', $report['location_n'], FALSE);
+        $this -> db ->set('photo', $report['photo'], TRUE);
+        $this -> db ->set('gps_accuracy', $report['gps_accuracy'], TRUE);
+        $this -> db ->insert('issues');
+         
+        //$ids = "id = 116";
+        //$this -> db ->select('location_n');
+        //$this -> db ->select('timestamp_n');
+        //$this -> db -> from('issues');
+        //$this -> db ->where('id"="11');
+        //$query = $this -> db -> query("SELECT ST_AsGeoJSON(location_n) as rt FROM issues WHERE id =15");
+       
+        
+        //$query = $this->db->get();
+        //var_dump($query);
+        //$data = ob_get_clean();
+        //file_put_contents("./uploads/info2.txt",$data);
+        /*
+        $arr_properties=null;
+        $geometry = null;
+        
+         */
+            
+            
+        
+            
+        //}
+          
+         
+        
+        /*
+         $query = $this -> db -> query("SELECT row_to_json(fc) FROM
+                                      (SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features
+                                      FROM (SELECT 'Feature' As type
+                                      , ST_AsGeoJSON(location_n)::json As geometry
+                                      , row_to_json((SELECT l FROM (SELECT timestamp_n,id) As l
+                                      )) As properties
+                                      FROM issues As lg ) As f) As fc");
+
+        */
+        //$jdata = ob_get_clean();
+       return TRUE;
+     }
+    }
+?>
